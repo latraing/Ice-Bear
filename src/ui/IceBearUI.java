d6aca45a5ed5fa7852db4a7b2795813412a634e7
latraing <latraing@ncsu.edu>
2020-09-02 19:05:18 -0400

    Working main method.

--- src/ui/IceBearUI.java
+++ src/ui/IceBearUI.java
@@ -1,5 +1,18 @@
 package ui;
 
-public class IceBearUI {
+import javax.security.auth.login.LoginException;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.OnlineStatus;
+
+public class IceBearUI {
+	public static JDA jda;
+	
+	public static void main (String[] args) throws LoginException {
+		jda = JDABuilder.createDefault("NzUwODE2OTY4ODY4NjI2NDYy.X1ACyg.cVLcWjFQVzV7HNkYzuJssuRX_f4").build();
+		jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.watching("We Bare Bears"));;
+		
+	}
 }
